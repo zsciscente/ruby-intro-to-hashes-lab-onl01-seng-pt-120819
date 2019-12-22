fd410bed6d58fc8fb8f6c6ff240285e6e44182d4
Zachary Sciscente <zsciscente@gmail.com>
2019-12-22 22:49:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,10 @@ def monopoly_with_second_tier
   
 	monopoly = {
 	  railroad: {
-	    pieces: "4", names: {}, rent_in_dollars: {} }
+	    pieces: "4",
+	    names: {},
+	    rent_in_dollars: {}
+	}
 
 
 end
