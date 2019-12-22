fab239533dd263d40b4b971d461cddebba2d6d26
Zachary Sciscente <zsciscente@gmail.com>
2019-12-22 22:58:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,12 +20,7 @@ def monopoly_with_second_tier
   {
     :pieces=>4,
     :rent_in_dollars=>
-    {
-      :one_piece_owned=>25,
-      :two_pieces_owned=>50,
-      :three_pieces_owned=>100,
-      :four_pieces_owned=>200
-    },
+    {},
     :names=> { }
   }
 }
